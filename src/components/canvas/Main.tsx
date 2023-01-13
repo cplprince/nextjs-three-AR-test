db99f9d341f3e0b79883ee3f4e342f35982c7165
Tomáš Endrych <tomas.endrych@superkoders.com>
2023-01-13 13:42:47 +0100

    FInall

--- src/components/canvas/Main.tsx
+++ src/components/canvas/Main.tsx
@@ -1,13 +1,15 @@
 import { ZapparCamera, ImageTracker } from '@zappar/zappar-react-three-fiber'
 import { useState } from 'react'
 import Card from './Card'
+import { Statue } from './Statue'
 
 const Main = ({ ...props }) => {
   const [visible, setVisible] = useState(false)
+  const [visibleStatue, setVisibleStatue] = useState(false)
 
   return (
     <>
-      <ZapparCamera userCameraMirrorMode='none' rearCameraMirrorMode='poses' />
+      <ZapparCamera userCameraMirrorMode='none' rearCameraMirrorMode='none' />
       <ImageTracker
         onNotVisible={(anchor) => setVisible(false)}
         onNewAnchor={(anchor) => setVisible(true)}
@@ -15,13 +17,13 @@ const Main = ({ ...props }) => {
         targetImage={'/assets/BusinessCard.zpt'}>
         <Card visible={visible} />
       </ImageTracker>
-      {/* <ImageTracker
-        onNotVisible={(anchor) => setVisible(false)}
-        onNewAnchor={(anchor) => setVisible(true)}
-        onVisible={(anchor) => setVisible(true)}
-        targetImage={'/assets/BusinessCard.zpt'}>
-        <Card visible={visible} />
-      </ImageTracker> */}
+      <ImageTracker
+        onNotVisible={(anchor) => setVisibleStatue(false)}
+        onNewAnchor={(anchor) => setVisibleStatue(true)}
+        onVisible={(anchor) => setVisibleStatue(true)}
+        targetImage={'/assets/statue.zpt'}>
+        <Statue visible={visibleStatue} />
+      </ImageTracker>
     </>
   )
 }
--- src/components/canvas/Scene.tsx
+++ src/components/canvas/Scene.tsx
@@ -1,10 +1,25 @@
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls, Preload } from '@react-three/drei'
+import { ResizeObserver } from '@juggle/resize-observer'
+import { useEffect, useState } from 'react'
 
 export default function Scene({ children, ...props }) {
   // Everything defined in here will persist between route changes, only children are swapped
+  const [DPR, setDPR] = useState(null)
+
+  useEffect(() => {
+    setDPR(window.devicePixelRatio)
+  }, [])
+
   return (
-    <Canvas {...props}>
+    <Canvas
+      flat
+      resize={{ polyfill: ResizeObserver }}
+      linear
+      dpr={DPR}
+      id='zapparCanvas'
+      {...props}
+      gl={{ antialias: false, ...props.gl }}>
       <directionalLight intensity={0.75} />
       <ambientLight intensity={0.75} />
       {children}
